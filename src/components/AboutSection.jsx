8fffe9c4e0d20d8eb954c8c43ed4d5a023c8a3ef
MdAyanBadar <158168631+MdAyanBadar@users.noreply.github.com>
2025-09-01 18:11:59 +0530

    Added resume PDF

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -34,7 +34,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="public/projects/MDAyanBadar_resume.pdf"
+                href="public/projects/MD_Ayan_Badar_Resume.pdf"
                 download="MD_Ayan_Badar_Resume.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
